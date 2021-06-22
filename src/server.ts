38cc504de394237e50c5437cfcdb8a6f3a709a82
usertest <emaildofulano@gmail.com>
2021-06-21 23:19:32 -0300

    Aula 2 Parte 1

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,10 @@
+import 'reflect-metadata';
 import express from 'express';
+
+import './database' //Importando index.ts (padrão)
 
 //Inicializando Express
 const app = express();
-
-app.get('/test', (req, res) => {
-  return res.send('Olá mundo - Requisição GET')
-})
-
-app.post('/test-post', (req, res) => {
-  return res.send('Olá mundo - Requisição POST')
-})
 
 //http://localhost:3000
 app.listen(3000, () => console.log('Server is running'))
